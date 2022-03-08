c2135b739915fb760fb674029e90015060502d74
Jian Earle <jianearle93@googlemail.com>
2022-03-08 22:47:47 +0000

    install.sh refactor - Check shell variable for zsh or bash and function for updating path in shell config

--- install.sh
+++ install.sh
@@ -1,32 +1,49 @@
 #!/usr/bin/env bash
 
 # Check to see if command has been installed and if not cp to user's bin directory
-if [ -e $HOME/bin/command ]
+if [ -e $HOME/bin/mkcommand ]
 then
 	echo "command is already installed"
 else
+
+  # Check if this exists first - TODO
   mkdir -p $HOME/bin
 
   # Add bin directory to path (bash)
-  if [ -f ~/.bashrc ]
+  if [[ $SHELL == *"bash"* ]]
   then
-    echo PATH=$PATH:$HOME/bin >> ~/.bashrc
-  else
-    touch ~/.bashrc
+     bash_config="$HOME/.bashrc"
+     updatePath $bash_config
   fi
 
   # Add bin directory to path (zsh)
-  if [ -f ~/.zshrc ]
+  if [[ $SHELL == *"zsh"* ]]
   then
-     echo PATH=$PATH:$HOME/bin >> ~/.zshrc
-  else
-    touch ~/.zshrc
+     zsh_config=$HOME/".zshrc"
+     updatePath $zsh_config
   fi
 
- 	cp ./command.sh $HOME/bin/command
-	chmod +x $HOME/bin/command
+  # Move command into bin directory
+  echo "Copying command into bin directory"
+ 	cp ./command.sh $HOME/bin/mkcommand
+	chmod +x $HOME/bin/mkcommand
+fi
+
+  # Check installation completed
+if [ -e $HOME/bin/mkcommand ]
+then
+  echo "Command installed!"
 fi
 
 
+# Check the user's path and update if needed
+function updatePath () {
+  if [[ ":$PATH:" == *":$HOME/bin:"* ]]; then
+    echo "~/bin is already on path.."
+  else
+    echo "Adding ~/bin to path."
+    echo "export PATH=$PATH:$HOME/bin" >> $1
+  fi
+}
 
 
