aa484850733fd89bcff7585934f65e5e0b889528
Jian Earle <jianearle93@googlemail.com>
2022-03-09 23:45:07 +0000

    Wrapped install script in function and used curl command to download src files

--- install.sh
+++ install.sh
@@ -1,43 +1,45 @@
 #!/usr/bin/env bash
 
+function install (){
 # Check to see if command has been installed and if not cp to user's bin directory
-if [ -e $HOME/bin/mkcommand ]
-then
-	echo "command is already installed"
-else
+  if [ -e $HOME/bin/mkcommand ]
+  then
+  	echo "command is already installed"
+  else
 
   # Check if this exists first - TODO
-  mkdir -p $HOME/bin
+    mkdir -p $HOME/bin
 
-  # Add bin directory to path (bash)
-  if [[ $SHELL == *"bash"* ]]
-  then
-     bash_config="$HOME/.bashrc"
-     updatePath $bash_config
-  fi
+    # Add bin directory to path (bash)
+    if [[ $SHELL == *"bash"* ]]
+    then
+       bash_config="$HOME/.bashrc"
+       update_path $bash_config
+    fi
 
   # Add bin directory to path (zsh)
-  if [[ $SHELL == *"zsh"* ]]
-  then
-     zsh_config=$HOME/".zshrc"
-     updatePath $zsh_config
-  fi
+    if [[ $SHELL == *"zsh"* ]]
+    then
+       zsh_config=$HOME/".zshrc"
+       update_path $zsh_config
+    fi
 
   # Move command into bin directory
-  echo "Copying command into bin directory"
- 	cp ./command.sh $HOME/bin/mkcommand
-	chmod +x $HOME/bin/mkcommand
-fi
+    echo "Downloading src files from github"
+    curl -s https://raw.githubusercontent.com/jearle10/command/main/command.sh > $HOME/bin/mkcommand
+  	chmod +x $HOME/bin/mkcommand
+  fi
 
   # Check installation completed
-if [ -e $HOME/bin/mkcommand ]
-then
-  echo "Command installed!"
-fi
+  if [ -e $HOME/bin/mkcommand ]
+  then
+    echo "Command installed!"
+  fi
+}
 
 
 # Check the user's path and update if needed
-function updatePath () {
+function update_path () {
   if [[ ":$PATH:" == *":$HOME/bin:"* ]]; then
     echo "~/bin is already on path.."
   else
@@ -45,5 +47,7 @@ function updatePath () {
     echo "export PATH=$PATH:$HOME/bin" >> $1
   fi
 }
+
+install
 
 
