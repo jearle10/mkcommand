acce4edd5b5e2f61f95c71acc78d2673b742172c
Jian Earle <jianearle93@googlemail.com>
2022-07-16 01:26:45 +0100

    Refactoring

--- src/command.sh
+++ src/command.sh
@@ -1,67 +1,76 @@
 #!/usr/bin/env bash
 
-
-# Check if a file has been entered
-if [ -z $1 ]
-then 
-	echo "Missing filepath"
-	exit 1
-fi
-
-
 # Get the filepath of the script to make executable
 filePath=$1
 
-
-# Remove the file extension and create command name
-command=$(basename -s .sh $filePath | basename -s .py $filepath | basename -s .js $filepath)
-
-echo "Making command called $command"
-
-
-# Check if the script already exists in bin and notify user 
-if [ -e $HOME/bin/$fileName ]
-then 
-	printf "\nfile already exists..overwriting\n"
-fi
+# Check if a file has been entered
+  if [ -z $1 ]
+  then 
+    echo "Missing filepath"
+    exit 1
+  fi
 
 
-# Copy the script into bin dir on user's path
-cp $filePath $HOME/bin/$command
+function main(){
+
+  # Remove the file extension and create command name
+  command=$(basename $1 .sh)
+  command=$(basename $command .py)
+  command=$(basename $command .js)
+
+  echo "Making command called $command"
 
 
-# Make the script executable
-chmod +x $HOME/bin/$command
+  # Check if the script already exists in bin and notify user 
+  if [ -e $HOME/bin/$fileName ]
+  then 
+    printf "\nfile already exists..overwriting\n"
+  fi
 
 
-if [ -x $HOME/bin/$fileName ]
-then
-	printf "\nFile is executable\n"
-fi
+  # Copy the script into bin dir on user's path
+  cp $1 $HOME/bin/$command
 
-if [[ "$command"=="*.sh" ]]; then
-  echo "Creating alias for $command"
-  create_alias $command
-fi
+
+  # Make the script executable
+  chmod +x $HOME/bin/$command
+
+
+  if [ -x $HOME/bin/$fileName ]
+  then
+    printf "\nFile is executable\n"
+  fi
+
+  if [[ "$1" == *".sh" ]]; then
+    echo "Creating alias for $command"
+    create_alias $command
+  fi
+}
 
 # Create an alias for the command so it doesnt need to be sourced
 function create_alias () {
+
+  command=$1
+
   # Add command alias (bash)
   if [ -f ~/.bashrc ]
   then
-    echo "alias $1='source $1'" >> ~/.bashrc
+    echo "alias $command='source $command'" >> ~/.bashrc
   else
     touch ~/.bashrc
-	echo "alias $1='source $1'" >> ~/.bashrc
+	echo "alias $command='source $command'" >> ~/.bashrc
   fi
 
   # Add command alias (zsh)
   if [ -f ~/.zshrc ]
   then
-     echo "alias $1='source $1" >> ~/.zshrc
+     echo "alias $command='source $command" >> ~/.zshrc
   else
     touch ~/.zshrc
-	echo "alias $1='source $1'">> ~/.zshrc
+	echo "alias $command='source $command'">> ~/.zshrc
   fi
 
 }
+
+
+main $1
